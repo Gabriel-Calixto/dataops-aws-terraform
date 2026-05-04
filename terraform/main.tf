3899df974474ff5b331b8201d04170398525b3d2
Gabriel Calixto <gabriel.calixto@al.infnet.edu.br>
2026-05-04 18:58:12 -0300

    configura buildspec para pipeline CI/CD DataOps

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,18 @@
 terraform {
+  backend "s3" {
+    bucket = "cloudmart-terraform-state-gabriel-calixto"
+    key    = "cloudmart/terraform.tfstate"
+    region = "us-east-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
